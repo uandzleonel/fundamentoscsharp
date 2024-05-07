5ef5de72d00ab0fb31c51250522ff25c75f935e9
Uanderson Leonel <dev.uandz@gmail.com>
2024-05-07 19:02:27 -0300

    First commit

---
+++ Car.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoConsole;
+class Car
+{
+    public void Ligar()
+    {
+        Console.WriteLine("Carro LIGADO");
+    }
+    
+    public void Desligar()
+    {
+        Console.WriteLine("Carro DESLIGADO");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ProjetoConsole;
+
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine($"Arg[{i}] = [{args[i]}]");
+        }
+
+        Car car = new Car();
+
+        car.Ligar();
+                
+        string nome = "Uanderson";
+        Console.WriteLine(nome);
+
+        Console.ReadKey();
+    }
+}
